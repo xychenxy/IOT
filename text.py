f79cd8425bf70eff199be961cf4ea595c53e7599
xychenxy <mavischen916@gmail.com>
2019-03-05 10:08:23 +1100

    Initial commit

---
+++ text.py
@@ -0,0 +1,20 @@
+
+from sense_hat import SenseHat
+import time
+
+sense = SenseHat()
+
+sense.clear()
+
+
+
+sense.set_pixel(1, 6, 255, 0, 0)
+sense.set_pixel(6, 6, 0, 255, 0)
+sense.set_pixel(2, 5, 0, 0, 255)
+sense.set_pixel(3, 5, 255, 0, 255)
+sense.set_pixel(3, 6, 255, 0, 255)
+sense.set_pixel(4, 6, 255, 0, 255)
+sense.set_pixel(7, 7, 255, 0, 255)
+
+
+
